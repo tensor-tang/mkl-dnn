f2b575f9ca4865c40037da34078ead9df5e82dd5
tensor-tang <jian.j.tang@intel.com>
2018-03-17 15:32:42 +0800

    Revert "common: introduce 3d convolution"
    This reverts commit 1a4d45d90a3889a08a857d5896f61e23be5a50b4.

--- tests/benchdnn/conv/bench_conv.cpp
+++ tests/benchdnn/conv/bench_conv.cpp
@@ -27,6 +27,8 @@
 
 #include "conv/conv.hpp"
 
+#include "conv/input_conv.hpp"
+
 namespace conv {
 
 /* global driver parameters */
@@ -117,6 +119,14 @@ int bench(int argc, char **argv, bool main_bench) {
             }
             check_correctness(&c);
         }
+    }
+
+    /* deprecated? */
+    if (main_bench && benchdnn_stat.tests == 0) {
+        /* use default list of problems */
+        int N = sizeof(default_list) / sizeof(default_list[0]);
+        for (int n = 0; n < N; ++n)
+            check_correctness(&default_list[n]);
     }
 
     return OK;
